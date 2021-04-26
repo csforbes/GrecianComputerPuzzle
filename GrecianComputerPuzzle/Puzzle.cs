5bab4829f95132d50afeb54ed04fa62f99c7a54f
Caleb Forbes <caleb.s.forbes@gmail.com>
2021-04-26 16:40:02 -0500

    successfully return Face Values

--- GrecianComputerPuzzle/Puzzle.cs
+++ GrecianComputerPuzzle/Puzzle.cs
@@ -3,7 +3,7 @@ namespace GrecianComputerPuzzle
 {
     public class Puzzle
     {
-        private int?[][,] _dials = new int?[][,]
+        private int?[][,] dials = new int?[][,]
         {
             new int?[,] //innermost dial, rotating counterclockwise
             {
@@ -36,42 +36,34 @@ namespace GrecianComputerPuzzle
             }
         };
 
-        //innermost dial, rotating counterclockwise
-        private int?[,] Dial1 =
-        { { 10, null, 7, null, 15, null, 8, null, 3, null, 6, null } };
+        public int Circumference { get { return dials[0].GetLength(1); }}
 
-        //second dial down, starting from the innermost ring
-        private int?[,] Dial2 =
-        {
-            { 17, 7, 3, null, 6, null, 11, 11, 6, 11, null, 6 },
-            { null, 4, null, 7, 15, null, null, 14, null, 9, null, 12 }
-        };
+        public int Diameter { get { return dials[dials.Length - 1].GetLength(0); }}
 
-        //third dial down
-        private int?[,] Dial3 =
+        public int[,] GetFaceValues()
         {
-            { 21, 17, 4, 5, null, 7, 8, 9, 13, 9, 7, 13 },
-            { 18, 11, 26, 14, 1, 12, null, 21, 6, 15, 4, 9 },
-            { null, 22, null, 16, null, 9, null, 5, null, 10, null, 8 }
-        };
+            
+            int[,] faceValues = new int[Diameter, Circumference];
+            //for each place in faceValues, get the top available number at that level
+            for (int y = 0; y < Diameter; y++)
+            {
+                for (int x = 0; x < Circumference; x++)
+                {
+                    int depth = y + 0;
+                    int? value = dials[depth][y, x];
+                    while (value is null)
+                    {
+                        depth++;
+                        value = dials[depth][y, x];
+                    }
+                    faceValues[y, x] = (int)value;
+                }
+            }
+            return faceValues;
+        }
 
-        //fourth dial down
-        private int?[,] Dial4 =
-        {
-            { 16, 2, 7, null, 9, null, 7, 14, 11, null, 8, null },
-            { 9, null, 9, 20, 12, 3, 6, null, 14, 12, 3, 8 },
-            { 3, 12, 3, 26, 6, null, 2, 13, 9, null, 17, 19 },
-            { 10, null, 1, null, 9, null, 12, null, 6, null, 10, null }
-        };
-
-        //fifth dial, bottom
-        private int?[,] Dial5 =
-        {
-            { 11, 14, 11, 11, 14, 11, 14, 11, 14, 14, 11, 14 },
-            { 14, 15, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 },
-            { 9, 9, 4, 4, 6, 6, 3, 3, 14, 14, 21, 21 },
-            { 7, 8, 8, 3, 4, 12, 2, 5, 10, 7, 16, 8 }
-        };
+        //public void RotateDial(int dial, bool direction, int places)
+        
 
         public Puzzle()
         {
