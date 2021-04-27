76754dc8189a6f33933432af58a8826335f0de40
Caleb Forbes <caleb.s.forbes@gmail.com>
2021-04-27 15:33:10 -0500

    GetColumn and Verify Column in Program
    since puzzle requirements are to add up the total of a vertical column of numbers, write Program methods to get a specified column and verify it against a target.

--- GrecianComputerPuzzle/Program.cs
+++ GrecianComputerPuzzle/Program.cs
@@ -1,16 +1,55 @@
 ﻿using System;
+using System.Linq;
 
 namespace GrecianComputerPuzzle
 {
     class Program
     {
+        private static int[] GetColumn(int[][] faceValues, int col)
+        {
+            int[] column = new int[faceValues.Length];
+            for (int row = 0; row < faceValues.Length; row++) //change to for st.
+            {
+                column[row] = faceValues[row][col];
+            }
+            return column;
+        }
+
+        private static bool VerifyColumn(int[] column, int targetSum)
+        {
+            if (targetSum == column.Sum())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         static void Main(string[] args)
         {
             Puzzle pozzle = new Puzzle();
-            int[][] pizzle = pozzle.GetFaceValues();
+            int[][] faceValues = pozzle.GetFaceValues();
+            int[] column = GetColumn(faceValues, 0);
+
+
+
+
+            foreach (int num in column)
+            {
+                Console.Write("{0} ", num);
+            }
+            Console.WriteLine();
             pozzle.RotateDialCounterClockwise(0, 1);
             pozzle.RotateDialCounterClockwise(1, 1);
-            pizzle = pozzle.GetFaceValues();
+            faceValues = pozzle.GetFaceValues();
+            column = GetColumn(faceValues, 0);
+            foreach (int[] row in faceValues)
+            {
+                Console.WriteLine(String.Join(" ", row));
+            }
+            Console.WriteLine(String.Join(" ", column));
         }
     }
 }
